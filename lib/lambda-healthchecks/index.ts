5fa7e5c8315d83e6b70d644d5e2209664ffc25da
psmyrdek <przemek.smyrdek@gmail.com>
2024-03-21 22:55:55 +0100

    feat: two distinct healthchecks

--- lib/lambda-healthchecks/index.ts
+++ lib/lambda-healthchecks/index.ts
@@ -11,19 +11,32 @@ interface LambdaResponse {
   body: string;
 }
 
+const WEBHOOK_URL =
+  'https://hooks.slack.com/services/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';
+
 export const handler: Handler<EventInput, LambdaResponse> = async (event) => {
   const { url, selector } = event;
   const { data: htmlContent } = await axios.get<string>(url);
   const hasSelector = checkForSelector(htmlContent, selector);
-  console.log(`Selector ${selector} ${hasSelector ? 'found' : 'not found'} at ${url} `);
+  await sendSlackMessage(url, hasSelector);
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'HTML content fetched',
-      hasSelector,
-    }),
+    body: `Lamba executed successfully.`,
   };
 };
+
+async function sendSlackMessage(appUrl: string, healthcheckResult: boolean) {
+  await axios.post(WEBHOOK_URL, {
+    text: `AWS Healtcheck for: ${appUrl}`,
+    attachments: [
+      {
+        fallback: `Result - ${healthcheckResult ? 'Success' : 'Failure'}`,
+        text: `Result - ${healthcheckResult ? 'Success' : 'Failure'}`,
+        color: healthcheckResult ? 'good' : 'danger',
+      },
+    ],
+  });
+}
 
 function checkForSelector(htmlContent: string, selector: string) {
   return htmlContent.includes(selector);
