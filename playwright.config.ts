d15d95e183f2db43ed26825dc221cfb41a5dbdc7
psmyrdek <przemek.smyrdek@gmail.com>
2024-03-22 12:56:42 +0100

    feat: e2e on staging

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests/e2e',
   fullyParallel: true,
@@ -17,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.E2E_URL || 'http://localhost:3000',
     trace: 'on-first-retry',
   },
   projects: [
@@ -25,42 +16,5 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
--- tests/e2e/tests/details.spec.ts
+++ tests/e2e/tests/details.spec.ts
@@ -2,13 +2,13 @@ import { test } from '@playwright/test';
 import { CharactersPage } from '../pages/CharactersPage';
 import { CharacterDetailsPage } from '../pages/CharacterDetailsPage';
 
-test('navigation to character details', async ({ page }) => {
+test('navigation to character details', { tag: '@list' }, async ({ page }) => {
   const mainPage = new CharactersPage(page);
   await mainPage.navigate();
   await mainPage.navigateToCharacterDetails('1');
 });
 
-test('navigation to characters list', async ({ page }) => {
+test('navigation to characters list', { tag: '@details' }, async ({ page }) => {
   const detailsPage = new CharacterDetailsPage(page, '3');
   await detailsPage.navigate();
   await detailsPage.navigateToCharactersList();
