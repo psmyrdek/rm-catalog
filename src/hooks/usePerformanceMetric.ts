e29ef155afe3bf086b32558ab0b3702c8a53a90b
psmyrdek <przemek.smyrdek@gmail.com>
2024-03-23 16:00:45 +0100

    feat: metric

--- src/hooks/usePerformanceMetric.ts
+++ src/hooks/usePerformanceMetric.ts
@@ -1,7 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export function usePerformanceMetric<T>(dep: T) {
+  const renderStart = useRef(0);
+
   useEffect(() => {
-    console.log(dep);
+    renderStart.current = performance.now();
+  }, []);
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_AWS_MONITORING_API, {
+      method: 'POST',
+      body: JSON.stringify({
+        loadTimeMs: performance.now() - renderStart.current,
+      }),
+    });
   }, [dep]);
 }
