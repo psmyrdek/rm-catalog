9d633db5a82c772671f84ca0db1e995793f450f0
psmyrdek <przemek.smyrdek@gmail.com>
2024-03-24 11:43:42 +0100

    fix: tf

--- terraform/modules/perf/lambda.tf
+++ terraform/modules/perf/lambda.tf
@@ -1,5 +1,5 @@
 data "external" "lambda_builder_sh" {
-  program = ["bash", "-c", "cd ${var.lambda_source_dir} && npm run build > '/dev/null' 2>&1 && echo \"{ }\" "]
+  program = ["bash", "-c", "cd ${var.lambda_source_dir} && npm install > '/dev/null' 2>&1 && npm run build > '/dev/null' 2>&1 && echo \"{ }\" "]
 
   query = {
     lambda_source_dir = var.lambda_source_dir
