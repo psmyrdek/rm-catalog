8a3216d8560a09d5d6619926ad8f12671b7a4313
psmyrdek <przemek.smyrdek@gmail.com>
2024-03-26 11:18:35 +0100

    feat: update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { useStatus } from '@featurevisor/react';
 
 const App = () => {
+  const { isReady } = useStatus();
+
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty!</h1>
       </div>
-      <Outlet />
+      {isReady && <Outlet />}
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-
 import { initSentry } from './sentry';
 import { createInstance } from '@featurevisor/sdk';
 import { FeaturevisorProvider } from '@featurevisor/react';
@@ -11,7 +10,7 @@ initSentry();
 
 const envName = import.meta.env.VITE_ENV_NAME || 'preview';
 const featurevisor = createInstance({
-  datafileUrl: `https://dqgfwd4vr1p9k.cloudfront.net/datafiles/${envName}/datafile-tag-all.json`,
+  datafileUrl: `https://d2vg9ga31syu7p.cloudfront.net/datafiles/${envName}/datafile-tag-all.json`,
 });
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
