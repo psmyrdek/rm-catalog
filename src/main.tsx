e2488f3ec35fd0e449239ca3a5663cf5b22fad9d
psmyrdek <przemek.smyrdek@gmail.com>
2024-03-26 11:34:16 +0100

    feat: temp remove

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { initSentry } from './sentry';
-import { createInstance } from '@featurevisor/sdk';
-import { FeaturevisorProvider } from '@featurevisor/react';
 import router from './router';
 
 initSentry();
 
-const envName = import.meta.env.VITE_ENV_NAME || 'preview';
-const featurevisor = createInstance({
-  datafileUrl: `https://d2vg9ga31syu7p.cloudfront.net/datafiles/${envName}/datafile-tag-all.json`,
-});
-
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
-    <FeaturevisorProvider instance={featurevisor}>
-      <RouterProvider router={router} />
-    </FeaturevisorProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -1,14 +1,9 @@
 import { Link, useLoaderData } from 'react-router-dom';
 import { CharacterListResponse } from '../../lib/rick-and-morty-api-client';
 import CharactersPagination from './CharactersPagination';
-import { useFlag } from '@featurevisor/react';
 
 const Characters = () => {
   const { info, results: characters } = useLoaderData() as CharacterListResponse;
-
-  const isPaginationEnabled = useFlag('pagination', {
-    country: new URLSearchParams(window.location.search).get('country'),
-  });
 
   return (
     <div className="bg-white rounded-lg shadow-sm p-4" data-testid="characters-list">
@@ -32,7 +27,7 @@ const Characters = () => {
           </li>
         ))}
       </ul>
-      {isPaginationEnabled && <CharactersPagination {...info} />}
+      <CharactersPagination {...info} />
     </div>
   );
 };
